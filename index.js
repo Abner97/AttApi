f4f45ba3e2e4c0682a05e0a4d1faf63419a8c5b8
Abraham Vega <abrahamvega987@gmail.com>
2020-05-16 01:20:58 -0500

    Se agrego soporte para jwt

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const oracle_queries_1 = require("./oracle_queries");
+const llave = require('./configs/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
@@ -20,8 +21,10 @@ const config = {
     connectString: 'localhost:1521/ORCLCDB.localdomain'
 };
 const q = new oracle_queries_1.queries(config);
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('llave', llave.llave);
+let datos = {};
 app.get('/portabilidad_gral', (req, res) => {
     (() => __awaiter(void 0, void 0, void 0, function* () {
         res.send(yield q.query("portabilidad_gral"));
@@ -45,6 +48,41 @@ app.get('/portabilidad_operador_out', (req, res) => {
 app.get('/portabilidad_operador_in', (req, res) => {
     (() => __awaiter(void 0, void 0, void 0, function* () {
         res.send(yield q.query("portabilidad_operador_in"));
+    }))();
+});
+app.get('/usuarios', (req, res) => {
+    console.log(req.body.user);
+    (() => __awaiter(void 0, void 0, void 0, function* () {
+        datos = yield q.query("usuarios", req.body.user, req.body.password);
+        //console.log(datos);
+        res.send(yield q.query("usuarios", req.body.user, req.body.password));
+    }))();
+});
+app.post('/autenticar', (req, res) => {
+    (() => __awaiter(void 0, void 0, void 0, function* () {
+        datos = yield q.query("autenticar", req.body.user, req.body.password);
+        // console.log(datos[0].USUARIO);
+        //console.log(datos[0].USUARIO);
+        try {
+            if (req.body.user === datos[0].USUARIO && req.body.password === datos[0].PASS) {
+                const payload = {
+                    check: true
+                };
+                const token = jwt.sign(payload, app.get('llave'), {
+                    expiresIn: 1440
+                });
+                res.json({
+                    mensaje: 'Autenticación correcta',
+                    token: token
+                });
+            }
+            else {
+                res.json({ mensaje: "Usuario o contraseña incorrectos" });
+            }
+        }
+        catch (err) {
+            res.json({ mensaje: "Usuario o contraseña incorrectos" });
+        }
     }))();
 });
 //PORT
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { queries } from './oracle_queries';
+const llave = require('./configs/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -14,39 +15,87 @@ const config = {
 
 const q = new queries(config);
 
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('llave',llave.llave);
 
+let datos: any = {};
 
-app.get('/portabilidad_gral',(req:any,res:any)=>{ //endpoint para portabilidad general
+app.get('/portabilidad_gral', (req: any, res: any) => { //endpoint para portabilidad general
     (async () => {
         res.send(await q.query("portabilidad_gral"));
-     })()
+    })()
 });
 
-app.get('/portabilidad_origen_out',(req:any,res:any)=>{ //endpoint para 
+app.get('/portabilidad_origen_out', (req: any, res: any) => { //endpoint para 
     (async () => {
         res.send(await q.query("portabilidad_origen_out"));
-     })()
+    })()
 });
 
-app.get('/portabilidad_lineal_origen_out',(req:any,res:any)=>{
+app.get('/portabilidad_lineal_origen_out', (req: any, res: any) => {
     (async () => {
         res.send(await q.query("portabilidad_lineal_origen_out"));
-     })()
+    })()
 });
 
-app.get('/portabilidad_operador_out',(req:any,res:any)=>{
+app.get('/portabilidad_operador_out', (req: any, res: any) => {
     (async () => {
         res.send(await q.query("portabilidad_operador_out"));
-     })()
+    })()
 });
 
-app.get('/portabilidad_operador_in',(req:any,res:any)=>{
+app.get('/portabilidad_operador_in', (req: any, res: any) => {
     (async () => {
         res.send(await q.query("portabilidad_operador_in"));
-     })()
+    })()
 });
+app.get('/usuarios', (req: any, res: any) => {
+    
+    console.log(req.body.user);
+
+
+    (async () => {
+        datos = await q.query("usuarios", req.body.user, req.body.password);
+        //console.log(datos);
+        res.send(await q.query("usuarios", req.body.user, req.body.password));
+    })()
+});
+
+
+
+app.post('/autenticar', (req: any, res: any) => {
+
+
+    (async () => {
+        datos = await q.query("autenticar", req.body.user, req.body.password);
+        // console.log(datos[0].USUARIO);
+         //console.log(datos[0].USUARIO);
+
+    try{
+        if (req.body.user === datos[0].USUARIO && req.body.password === datos[0].PASS) {
+            const payload = {
+                check: true
+            };
+            const token = jwt.sign(payload, app.get('llave'), {
+                expiresIn: 1440
+            });
+            res.json({
+                mensaje: 'Autenticación correcta',
+                token: token
+            });
+        } else {
+            res.json({ mensaje: "Usuario o contraseña incorrectos" });
+        }
+
+    }catch(err){
+        res.json({ mensaje: "Usuario o contraseña incorrectos" });
+    }
+   
+    })()
+   
+})
+
 
 
 
