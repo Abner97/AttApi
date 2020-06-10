99c0eaa5a68ed4151ba906b746f06be60b71b050
Abraham Vega <abrahamvega987@gmail.com>
2020-06-09 21:29:16 -0500

    cors

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ const corsOptions = {
     origin: 'http://localhost:4200',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 const rutasProtegidas = express.Router(); //middleware 
 rutasProtegidas.use((req, res, next) => {
     const token = req.headers['access-token'];
--- index.ts
+++ index.ts
@@ -11,7 +11,14 @@ const corsOptions = {
     origin: 'http://localhost:4200',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
-app.use(cors(corsOptions));
+
+
+  app.use(function(req:any, res:any, next:any) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 
 const rutasProtegidas = express.Router(); //middleware 
