42f76685d3d2aebbb73c5a9c5cf11e04b5191e13
Abraham Vega <abrahamvega987@gmail.com>
2020-06-09 20:47:19 -0500

    Adding cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const corsOptions = {
     origin: 'http://localhost:4200',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(cors(corsOptions));
+app.use('*', cors());
 const rutasProtegidas = express.Router(); //middleware 
 rutasProtegidas.use((req, res, next) => {
     const token = req.headers['access-token'];
