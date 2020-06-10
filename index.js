4493239ab44cd0faee73a4d5ddb04d8ee9a21d68
Abraham Vega <abrahamvega987@gmail.com>
2020-06-09 23:01:18 -0500

    cors

--- index.js
+++ index.js
@@ -21,16 +21,14 @@ const app = express();
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 // app.use(cors());
-// app.use(function (req:any, res:any, next:any) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header("Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-//     );
-//     if (req.method == 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//         return res.status(200).json({});
-//     } 
-// });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+});
 const rutasProtegidas = express.Router(); //middleware 
 rutasProtegidas.use((req, res, next) => {
     const token = req.headers['access-token'];
--- index.ts
+++ index.ts
@@ -13,16 +13,16 @@ const app = express();
 // }
 // app.use(cors());
 
-// app.use(function (req:any, res:any, next:any) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header("Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-//     );
-//     if (req.method == 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//         return res.status(200).json({});
-//     } 
-// });
+app.use(function (req:any, res:any, next:any) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+    );
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        return res.status(200).json({});
+    } 
+});
 
 
 const rutasProtegidas = express.Router(); //middleware 
