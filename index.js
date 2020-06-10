2a1885db79ec5bc3bb658d7ef19498eb2debf618
Abraham Vega <abrahamvega987@gmail.com>
2020-06-09 21:53:54 -0500

    corsexit

--- index.js
+++ index.js
@@ -21,12 +21,16 @@ const corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors());
-app.use((req, res, next) => {
+app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
 });
 const rutasProtegidas = express.Router(); //middleware 
 rutasProtegidas.use((req, res, next) => {
--- index.ts
+++ index.ts
@@ -10,37 +10,40 @@ const app = express();
 const corsOptions = {
     origin: 'http://localhost:4200',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-    app.use(cors());
+}
+app.use(cors());
 
-  app.use((req:any, res:any, next:any) => {
+app.all('*', function (req:any, res:any, next:any) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 
 const rutasProtegidas = express.Router(); //middleware 
-rutasProtegidas.use((req:any, res:any, next:any) => {
+rutasProtegidas.use((req: any, res: any, next: any) => {
     const token = req.headers['access-token'];
- 
+
     if (token) {
-      jwt.verify(token, app.get('llave'), (err:any, decoded:any) => {      
-        if (err) {
-          return res.json({ mensaje: 'Token inválida' });    
-        } else {
-          req.decoded = decoded;    
-          next();
-        }
-      });
+        jwt.verify(token, app.get('llave'), (err: any, decoded: any) => {
+            if (err) {
+                return res.json({ mensaje: 'Token inválida' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
     } else {
-      res.send({ 
-          mensaje: 'Token no proveída.' 
-      });
+        res.send({
+            mensaje: 'Token no proveída.'
+        });
     }
- });
+});
 
 
 const config = {//datos de acceso a la DB
@@ -54,41 +57,41 @@ const q = new queries(config);//creacion del objeto queries que da acceso a la b
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('llave',llave.llave);
+app.set('llave', llave.llave);
 
 let datos: any = {};
 
-app.get('/portabilidad_gral/:sysdate',rutasProtegidas, (req: any, res: any) => { //endpoint para portabilidad general
+app.get('/portabilidad_gral/:sysdate', rutasProtegidas, (req: any, res: any) => { //endpoint para portabilidad general
     (async () => {
-        res.send(await q.query("portabilidad_gral",null,null,null,null,null,req.params.sysdate));
+        res.send(await q.query("portabilidad_gral", null, null, null, null, null, req.params.sysdate));
     })()
 });
 
-app.get('/portabilidad_origen_out',rutasProtegidas, (req: any, res: any) => { //endpoint para 
+app.get('/portabilidad_origen_out', rutasProtegidas, (req: any, res: any) => { //endpoint para 
     (async () => {
         res.send(await q.query("portabilidad_origen_out"));
     })()
 });
 
-app.get('/portabilidad_lineal_origen_out/:sysdate',rutasProtegidas, (req: any, res: any) => {
+app.get('/portabilidad_lineal_origen_out/:sysdate', rutasProtegidas, (req: any, res: any) => {
     (async () => {
-        res.send(await q.query("portabilidad_lineal_origen_out",null,null,null,null,null,req.params.sysdate));
+        res.send(await q.query("portabilidad_lineal_origen_out", null, null, null, null, null, req.params.sysdate));
     })()
 });
 
-app.get('/portabilidad_operador_out',rutasProtegidas, (req: any, res: any) => {
+app.get('/portabilidad_operador_out', rutasProtegidas, (req: any, res: any) => {
     (async () => {
         res.send(await q.query("portabilidad_operador_out"));
     })()
 });
 
-app.get('/portabilidad_operador_in',rutasProtegidas, (req: any, res: any) => {
+app.get('/portabilidad_operador_in', rutasProtegidas, (req: any, res: any) => {
     (async () => {
         res.send(await q.query("portabilidad_operador_in"));
     })()
 });
-app.get('/usuarios',rutasProtegidas, (req: any, res: any) => {
-    
+app.get('/usuarios', rutasProtegidas, (req: any, res: any) => {
+
     console.log(req.body.user);
 
 
@@ -101,44 +104,44 @@ app.get('/usuarios',rutasProtegidas, (req: any, res: any) => {
 
 
 
-app.post('/autenticar',  cors(corsOptions),(req: any, res: any) => {//endpoint para autenticar al usuario
+app.post('/autenticar', cors(corsOptions), (req: any, res: any) => {//endpoint para autenticar al usuario
 
-    const plainPassword=req.body.password;
+    const plainPassword = req.body.password;
     const user = req.body.user;
     (async () => {
 
-    try{
+        try {
 
-        datos = await q.query("autenticar",null,null,null, user,null);
-        const passwordValidated = await q.validarPassword(plainPassword,datos[0].PASS);
+            datos = await q.query("autenticar", null, null, null, user, null);
+            const passwordValidated = await q.validarPassword(plainPassword, datos[0].PASS);
 
-        if (user === datos[0].USUARIO && passwordValidated) {
-            const payload = {
-                check: true
-            };
-            const token = jwt.sign(payload, app.get('llave'), {
-                expiresIn: 3600
-            });
-            res.json({
-                mensaje: 'Autenticación correcta',
-                token: token
-            });
-        } else {
+            if (user === datos[0].USUARIO && passwordValidated) {
+                const payload = {
+                    check: true
+                };
+                const token = jwt.sign(payload, app.get('llave'), {
+                    expiresIn: 3600
+                });
+                res.json({
+                    mensaje: 'Autenticación correcta',
+                    token: token
+                });
+            } else {
+                res.json({ mensaje: "Usuario o contraseña incorrectos" });
+            }
+
+
+        } catch (err) {
             res.json({ mensaje: "Usuario o contraseña incorrectos" });
         }
 
-       
-    }catch(err){
-        res.json({ mensaje: "Usuario o contraseña incorrectos" });
-    }
-   
     })();
-   
+
 });
 
-app.post('/registrar',rutasProtegidas,(req:any,res:any)=>{//endpoint para registrar al usuario
+app.post('/registrar', rutasProtegidas, (req: any, res: any) => {//endpoint para registrar al usuario
     (async () => {
-        res.json(await q.Registrar(req.body.name,req.body.lastName,req.body.email, req.body.user, req.body.password));
+        res.json(await q.Registrar(req.body.name, req.body.lastName, req.body.email, req.body.user, req.body.password));
     })()
 });
 
