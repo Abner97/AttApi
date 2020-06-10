2c27891e14f97169099c33815f22d25ec0d258ae
Abraham Vega <abrahamvega987@gmail.com>
2020-06-09 20:34:37 -0500

    Adding cors

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 var cors = require('cors');
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 const rutasProtegidas = express.Router(); //middleware 
 rutasProtegidas.use((req, res, next) => {
     const token = req.headers['access-token'];
--- index.ts
+++ index.ts
@@ -7,7 +7,11 @@ const jwt = require('jsonwebtoken');
 var cors = require('cors');
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+app.use(cors(corsOptions));
 
 
 const rutasProtegidas = express.Router(); //middleware 
